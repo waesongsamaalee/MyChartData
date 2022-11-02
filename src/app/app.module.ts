ad8858bb0ead2bbcdd5867d6cb67a90f2b08541d
waesongsamaalee <waesongsamaalee@gmail.com>
2022-11-02 10:52:16 +0700

    ส่งครั้งที่1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
@@ -20,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   imports: [
+    ChartsModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -7,6 +7,19 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+  // Doughnut
+public doughnutChartLabels:string[] = ['ปวช.1','ปวช.2','ปวช.3','ปวส.1','ปวส.2'];
+public doughnutChartData:number[] = [10,7,6,29,33];
+public doughnutChartType:string = 'doughnut';
+
+// events
+public chartClicked(e:any):void {
+  console.log(e);
+}
+
+public chartHovered(e:any):void {
+  console.log(e);
+}
   constructor(public navCtrl: NavController) {
 
   }
